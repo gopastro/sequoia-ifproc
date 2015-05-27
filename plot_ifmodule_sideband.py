22cc19cb5c362d8be2a05c6d52d0320ea56d9a3b
Aleksandar PopStefanija <aleks@labrabbit.(none)>
2015-05-27 10:29:12 -0400

    Added info for user setup

--- plot_ifmodule_sideband.py
+++ plot_ifmodule_sideband.py
@@ -2,6 +2,8 @@ import pandas as pd
 import itertools
 import math
 
+#needs user input at lines w/ ####
+
 marker = itertools.cycle((',', 'o', 'v', '^', '<',
                           '>', '1', '2', '3', '4',
                           's', 'p', '*', 'h', 'H',
@@ -12,25 +14,20 @@ colors = itertools.cycle(('b', 'g', 'r', 'c',
                           'firebrick', 'hotpink', 'khaki', 'maroon',
                           ))
 
-data = pd.read_csv('IFMod6_rev2_sidebandratio.txt', sep=',')
+data = pd.read_csv('IFMod7_rev2_sidebandratio.txt', sep=',') ####
 dg = data.groupby('P_in_adj')
 dgpow = dg.get_group(-9.0)
 fig = figure(figsize=(14, 8))
 dgifs = dgpow.groupby('IF_freq')
 
 for pixel in range(4):
-	pixelplus = pixel + 20 ####
+	pixelplus = pixel + 24 ####
         ax = fig.add_subplot(2, 2, pixel+1)
         iffreqs = dgifs.groups.keys()
         iffreqs.sort()
         for iffreq in iffreqs:
-            ifdata = dgifs.get_group(iffreq)
-	    #ydata = map(lambda x:(math.log(x+10)), 
-	    #		  getattr(ifdata, 'Dt_V%d' %pixelplus))
-	    if pixel==3:
-		ydata = getattr(ifdata, 'rat123')
-	    else:	
-	    	ydata = getattr(ifdata, 'rat%d' %pixelplus)
+            ifdata = dgifs.get_group(iffreq)	
+	    ydata = getattr(ifdata, 'rat%d' %pixelplus)
             ax.plot(ifdata['LO_freq'], ydata, marker=marker.next(), linestyle='-',                      color=colors.next(),label='IF = %d MHz' % iffreq)
         ax.set_title('Pixel %d' % (pixelplus))
         if pixel == 2:
@@ -40,6 +37,6 @@ for pixel in range(4):
 
 draw()
 show()
-fig.savefig('ifmodule6_rev2_img_rej.png') ####
+fig.savefig('ifmodule7_rev2_img_rej.png') ####
 
     
